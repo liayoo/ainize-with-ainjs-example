efd5dc19d1c5c4d122524212c6c881e78139b071
Lia Yoo <liayoo@Lias-MacBook-Pro.local>
2020-02-18 05:28:23 -0800

    Add another bot.

--- index.js
+++ index.js
@@ -3,8 +3,14 @@ const axios = require('axios');
 const Ain = require('@ainblockchain/ain-js').default;
 const get = require('lodash/get');
 const PORT = 80;
-const ANSWER_REF = '/apps/chatbots/papaya/response';
-const chatbotEndpoint = 'https://chatlearner.minho-comcom-ai.endpoint.ainize.ai/chatbot';
+const ANSWER_REF = '/apps/chatbots/';
+const papayaEndpoint = 'https://chatlearner.minho-comcom-ai.endpoint.ainize.ai/chatbot';
+const qnaEndpoint = 'https://qnabot-gpu.gkswjdzz.endpoint.dev.ainize.ai/chat';
+const botIdParasite = 'parasite_bongjunho';
+const botIdDemo = 'demo';
+const BONG_BOT_NAME = 'bonghive';
+const AIN_BOT_NAME = 'ai_network';
+const PAPAYA_BOT_NAME = 'papaya';
 
 const ain = new Ain('http://node.ainetwork.ai:8080');
 const addr = ain.wallet.add(process.env.PRIVATE_KEY);
@@ -27,23 +33,70 @@ app.get('/set_value', async (req, res) => {
 app.post('/', async (req, res) => {
   const tx = req.body.transaction;
   const question = get(tx, 'operation.value');
-  // console.log("i got a message:", question);
-  axios.get(chatbotEndpoint, {
-    params: {
-      sentence: question,
-      previous_session_id: 1
-    }
-  })
-  .then(async (response) => {
-    const answer = response.data.answer;
-    const ref = ain.db.ref(ANSWER_REF).push();
-    const result = await ref.setValue({ value: answer });
-    res.json(result);
-  })
-  .catch((error) => {
-    res.json(error.message);
-  })
+  let ref = get(tx, 'operation.ref');
+  const key = getKeyFromRef(ref);
+  if (!key) {
+    res.json({ error: `Invalid ref value received: ${ref}` });
+  } else {
+    // console.log("i got a message:", question);
+    let promises = [];
+    promises.push(axios.get(qnaEndpoint, {
+      params: {
+        bot_id: botIdParasite,
+        question: question
+      }
+    }))
+    promises.push(axios.get(qnaEndpoint, {
+      params: {
+        bot_id: botIdDemo,
+        question: question
+      }
+    }))
+    promises.push(axios.get(papayaEndpoint, {
+      params: {
+        sentence: question,
+        previous_session_id: 1
+      }
+    }))
+    await Promise.all(promises)
+    .then((responseList) => {
+      const bongRes = responseList[0].data;
+      const ainRes = responseList[1].data;
+      const papayaRes = responseList[2].data;
+      if (bongRes.okay === 'true') {
+        sendResponse(BONG_BOT_NAME, key, bongRes.answer, res);
+      } else if (ainRes.okay === 'true') {
+        sendResponse(AIN_BOT_NAME, key, ainRes.answer, res);
+      } else {
+        sendResponse(PAPAYA_BOT_NAME, key, papayaRes.answer, res);
+      }
+    })
+    .catch((error) => {
+      res.json(error);
+    })
+  }
 });
+
+function getKeyFromRef(ref) {
+  if (!ref) {
+    return null;
+  }
+  const refArr = ref.split('/');
+  const len = refArr.length;
+  if (refArr[len - 1] === '') {
+    if (len < 2) {
+      return null;
+    }
+    return refArr[len - 2];
+  }
+  return refArr[len - 1];
+}
+
+async function sendResponse(botName, key, answer, res) {
+  const ref = ain.db.ref(ANSWER_REF + botName + '/response/' + key);
+  const result = await ref.setValue({ value: answer });
+  res.json(result);
+}
 
 app.listen(PORT, () => {
   console.log(`Listening to requests on http://localhost:${PORT}`);
